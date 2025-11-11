3bb2292b8d467e6db55df42428d3f950291e2906
tanahjawa <jawafirst@gmail.com>
2025-11-11 21:27:25 +0700

    update

--- app_page1.py
+++ app_page1.py
@@ -5,7 +5,7 @@ def introduction_page():
     st.markdown("""
     <div style='text-align:center;'>
         <h1 style='color:#002C54;'>Deteksi Dini Risiko Kardiovaskular</h1>
-        <p style='font-size:18px;'>Aplikasi ini berfungsi sebagai skrining awal untuk membantu pengguna mengenali potensi risiko penyakit kardiovaskular (jantung dan pembuluh darah) serta meningkatkan kesadaran akan pentingnya menjaga kesehatan risiko tersebut — <b>bukan sebagai alat medis atau diagnosis yang pasti</b>.</p>
+        <p style='font-size:18px;'>Aplikasi ini berfungsi sebagai skrining awal untuk membantu pengguna mengenali potensi risiko penyakit kardiovaskular (jantung dan pembuluh darah) serta meningkatkan kesadaran akan pentingnya menjaga kesehatan risiko tersebut — <b>bukan alat diagnosis medis, melainkan alat prediksi berbasis AI</b>.</p>
     </div>
     """, unsafe_allow_html=True)
 
